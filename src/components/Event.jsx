53f7c1dbe5c1f58f125a9428360d0c4f35517b35
Michael Zeta <michael@a2zeta.com>
2022-07-27 01:48:55 -0700

    #38dvf2q[completed] Replaces date string dashes with slashes to resolve the bug

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -10,41 +10,45 @@ const Event = ({
   description,
   url,
 }) => {
-  // const eventMonth = date.slice(5, 7) //? Gets the month number from ISO date
-  //? Passes in the ISO date from Sanity and gets the month number.
-  //@ The +1 is because the Date().getMonth is 0 indexed and we want human numbers.
-  const eventDayNum = new Date(date).getDate() + 1
-  const eventMonth = new Date(date).toLocaleString('default', {
-    month: 'short',
-  })
+  //// const eventMonth = date.slice(5, 7) //? Gets the month number from ISO date
+  //- Passes in the ISO date from Sanity to determine the month number.
+  //? .replace(/-/g, '\/') is needed to solve the one day off issue from dates that fall on the first after a month with only 30 days (e.g. October 1st would show incorrectly as September 31st.)
+  //@ https://stackoverflow.com/questions/7556591/is-the-javascript-date-object-always-one-day-off
+  const eventDayNum = new Date(date.replace(/-/g, '/')).getDate()
+  const eventMonth = new Date(date.replace(/-/g, '/')).toLocaleString(
+    'default',
+    {
+      month: 'short',
+    }
+  )
 
   return (
-    <div className="event">
-      <div className="event-date">
-        <span className="event-month-small">{eventMonth}</span>
-        <span className="event-date-number">{eventDayNum}</span>
+    <div className='event'>
+      <div className='event-date'>
+        <span className='event-month-small'>{eventMonth}</span>
+        <span className='event-date-number'>{eventDayNum}</span>
       </div>
-      <hr className="show-on-mobile" />
-      <div className="event-title">{title ? title : city}</div>
-      <div className="event-venue">
-        <div className="event-venue-name">{venue}</div>
-        <div className="event-venue-address">
+      <hr className='show-on-mobile' />
+      <div className='event-title'>{title ? title : city}</div>
+      <div className='event-venue'>
+        <div className='event-venue-name'>{venue}</div>
+        <div className='event-venue-address'>
           <a
             href={`https://www.google.com/maps/search/${venue}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="venue-link"
+            target='_blank'
+            rel='noopener noreferrer'
+            className='venue-link'
           >
             {address}
           </a>
         </div>
       </div>
-      <div className="event-ticket-link">
+      <div className='event-ticket-link'>
         <a
           href={url ? url : `https://www.google.com/maps/search/${venue}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="button"
+          target='_blank'
+          rel='noopener noreferrer'
+          className='button'
         >
           Tickets
         </a>
